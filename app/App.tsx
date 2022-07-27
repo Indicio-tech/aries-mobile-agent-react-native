5564b16b6ca3c46a79f650925a2031a2b3ee57ff
KolbyRKunz <KolbyKunz@yahoo.com>
2022-07-27 11:54:43 -0600

    trying to add notifee to bifold
    Signed-off-by: KolbyRKunz <KolbyKunz@yahoo.com>

--- app/App.tsx
+++ app/App.tsx
@@ -21,6 +21,8 @@ import React, { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
 import SplashScreen from "react-native-splash-screen";
 import Toast from "react-native-toast-message";
+import messaging, { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
+import notifee from '@notifee/react-native'
 
 const defaultConfiguration: ConfigurationContext = {
   pages: OnboardingPages,
@@ -38,7 +40,30 @@ const App = () => {
     // Hide the native splash / loading screen so that our
     // RN version can be displayed.
     SplashScreen.hide();
+    registerNotifications();
   }, []);
+
+  const notificationHandler = async (message:FirebaseMessagingTypes.RemoteMessage) => {
+    console.log('received a message', message)
+    //trigger notification with notifee
+    notifee.displayNotification({
+      title: 'Something else',
+      body: 'custom notification',
+      android: {
+        channelId: 'defualt'
+      },
+    })
+  }
+
+  const registerNotifications = async () => {
+    console.log('The token is: ', await messaging().getToken())
+    messaging().setBackgroundMessageHandler(notificationHandler)
+    messaging().onMessage(notificationHandler)
+    await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+  }
 
   return (
     <StoreProvider>
